78dd192d85bbd97fa22999c82c3716121031e22f
Zakaria Jama <102694126+zakjama0@users.noreply.github.com>
2023-08-31 11:59:50 +0100

    Add files via upload

---
+++ portfolioscript.js
@@ -0,0 +1,7 @@
+// bar effects on scroll
+window.addEventListener("scroll", function(){
+
+    const header = document.querySelector("header");
+
+    header.classList.toggle('sticky',window.scrollY > 0)
+} )
